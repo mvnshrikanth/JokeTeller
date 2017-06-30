985f6c1f723c98a09a78ad02dc3e9cad3556a3b1
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-06-29 22:26:46 -0400

    Deployed the lib in Google Cloud

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -20,13 +20,22 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
     @Override
     protected String doInBackground(Void... params) {
         if (myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - set to test on a physical device with a local development server
-                    // (to get your ip address, go to cmd -> ipconfig -> IPV4 address with port number)
-                    .setRootUrl("http://192.168.1.114:8080/_ah/api/");
-            // end options for devappserver
+//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+// options for running against local devappserver
+// - set to test on a physical device with a local development server
+// (to get your ip address, go to cmd -> ipconfig -> IPV4 address with port number)
+//                    .setRootUrl("http://192.168.1.114:8080/_ah/api/");
+// end options for devappserver
+//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/") // 10.0.2.2 is localhost's IP address in Android emulator
+//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                        @Override
+//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                            abstractGoogleClientRequest.setDisableGZipContent(true);
+//                        }
+//                    });
+
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://jokeapptestapi.appspot.com/_ah/api/");
 
             myApiService = builder.build();
         }
--- jokelib/src/main/java/com/example/JokeProvider.java
+++ jokelib/src/main/java/com/example/JokeProvider.java
@@ -4,12 +4,16 @@ import java.util.Random;
 
 public class JokeProvider {
     private static final String[] jokes = {
-            "A SQL query goes into a bar, walks up to two tables and asks, 'Can I join you?'",
-            "How many programmers does it take to change a light bulb ? None, that's a hardware problem",
-            "Command line Russian roulette : [ $[ $RANDOM % 6 ] == 0 ] && rm -rf / || echo *Click*",
-            "In theory, there ought to be no difference between theory and practice. In practice, there is.",
-            "Why did the programmer quit his job? Because he didn't get arrays.",
-            "I � Unicode."
+            "Police officer talks to a driver: Your tail light is broken, your tires must be exchanged and your bumper hangs halfway down. That will be 300 dollars.\n" + "Driver: Alright, go ahead. They want twice as much as that at the garage.",
+            "Why did the physics teacher break up with the biology teacher? There was no chemistry.",
+            "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
+            "I forgot my cell phone when I went to the toilet yesterday. We have 245 tiles.",
+            "I’ve always thought my neighbors were quite nice people. But then they put a password on their Wi-Fi.",
+            "There are more English speakers in China than in the United States.",
+            "The struggle is real for the ferret – if a female ferret in heat doesn’t find a mate, she can die.",
+            "In the USA, there are more Chinese restaurants than all the McDonald’s, KFCs, Burger Kings and Wendy’s put together.",
+            "How can you tell you have a really bad case of acne?\n" + "It’s when the blind try to read your face.",
+            "Boy complains to his father: You told me to put a potato in my swimming trunks! You said it would impress the girls at the pool! But you forgot to mention one thing! \n" + "\n" + "Father: Really, what?\n" + "\n" + "Boy: That the potato should go in the front."
     };
 
     public static String getJoke() {
